bf63fdcd7278c8b2ff7415c5ad6652a2c7b0d2e6
Elliott Thompson <ellth92@gmail.com>
2015-08-02 22:32:17 +0100

    updated the build config to minify css and js

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,8 @@ var rename = require('gulp-rename');
 var reload = require('gulp-livereload');
 var minifyImg = require('gulp-imagemin');
 var minjpeg = require('imagemin-jpegtran');
-
+var autoprefixer = require('gulp-autoprefixer');
+var htmlreplace = require('gulp-html-replace');
 
 //tasks
 
@@ -34,13 +35,17 @@ gulp.task('dist-minify-css', function() {
     './app/**/bootstrap.min.css',
     './app/css/*.css'])
     .pipe(concat('bundle.min.css'))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
     .pipe(minifyCSS(opts))
     .pipe(gulp.dest('./dist/css/'))
 });
 
-gulp.task('minify-js', function() {
+gulp.task('dist-minify-js', function() {
   gulp.src([
-    './app/**/**/**/jquery.min.js',
+    './app/**/jquery.min.js',
     './app/**/bootstrap.min.js',
     './app/**/angular.min.js',
     './app/**/angular-route.min.js',
@@ -49,34 +54,21 @@ gulp.task('minify-js', function() {
     '!./app/**/config.js',
     '!./app/bower_components/**'])
     .pipe(concat('bundle.min.js'))
-    // .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('./app/js/'))
-});
-
-gulp.task('dist-minify-js', function() {
-  gulp.src([
-    './app/**/jquery.min.js',
-    './app/**/angular.min.js',
-    './app/**/angular-route.min.js',
-    './app/**/banner.js',
-    './app/**/main.js',
-    '!./app/**/config.js',
-    '!./app/bower_components/**'])
-    .pipe(concat('bundle.min.js'))
-    // .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('copy-bower-components', function() {
-  gulp.src('./app/js/vendor/**')
-    .pipe(gulp.dest('./dist/js/vendor/'));
-});
-
 gulp.task('copy-html-files', function() {
-  gulp.src('./app/**/*.html')
+  gulp.src(['./app/**/*.html','!./app/index.html'])
   .pipe(gulp.dest('dist/'));
+
+  gulp.src('./app/index.html')
+  .pipe(htmlreplace({
+    'css':'css/bundle.min.css',
+    'js':'js/bundle.min.js'
+  }))
+  .pipe(gulp.dest('dist/'));
+
 });
 
 gulp.task('copy-images', function() {
@@ -96,6 +88,15 @@ gulp.task('connect', function() {
   });
 });
 
+gulp.task('watch', function() {
+  reload.listen();
+  gulp.watch(['./app/**/*.html','./app/js/*.js','!./app/js/vendor/*.*','./app/css/*.css'],['livereload']);
+});
+
+gulp.task('livereload', function(){
+  reload.reload();
+});
+
 gulp.task('connectDist', function() {
   connect.server({
     root: 'dist/',
@@ -104,12 +105,7 @@ gulp.task('connectDist', function() {
 });
 
 //default task
-gulp.task('default',
-  ['lint','minify-js', 'connect'], function() {
-    reload();
-    gulp.watch(['./app/**/*.html','./app/js/*.js','!./app/js/vendor/*.*','./app/css/*.css']);
-  }
-);
+gulp.task('default', ['lint', 'connect','watch']);
 
 //build task
 gulp.task('build',
